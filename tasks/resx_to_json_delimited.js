cb7fb02dba614f6f70c06783b735d99b68de81eb
Dustin Cassiday <dustincassiday@Dustins-MacBook-Pro.local>
2016-10-24 17:56:25 -0400

    fixed formatting, package.json

--- tasks/resx_to_json_delimited.js
+++ tasks/resx_to_json_delimited.js
@@ -11,89 +11,90 @@
 
 module.exports = function (grunt) {
 
-	var xml2js = require('xml2js');
+  var xml2js = require('xml2js');
 
-	grunt.registerMultiTask(
-		'resx_to_json_delimited',
-		'Convert .resx files to multi-dimensional .json files using optional delimiter.',
-		multiTask);
+  grunt.registerMultiTask(
+    'resx_to_json_delimited',
+    'Convert .resx files to multi-dimensional .json files using optional delimiter.',
+    multiTask);
 
-	function multiTask() {
-		var opts = this.options(),
-			files = this.files,
-			dest = null;
+  function multiTask() {
+    var opts = this.options(),
+      files = this.files,
+      dest = null;
 
-		files.forEach(filesOp);
+    files.forEach(filesOp);
 
-		function filesOp(filePair) {
-			dest = filePair.dest;
-			filePair.src.forEach(fileOp);
-		}
+    function filesOp(filePair) {
+      dest = filePair.dest;
+      filePair.src.forEach(fileOp);
+    }
 
-		function fileOp(file) {
-			if (isResx(file)) { parser(file); }
-		}
+    function fileOp(file) {
+      if (isResx(file)) { parser(file); }
+    }
 
-		function parser(file) {
-			var buffer = grunt.file.read(file),
-				parser = new xml2js.Parser();
+    function parser(file) {
+      var buffer = grunt.file.read(file),
+        parser = new xml2js.Parser();
 
-			parser.parseString(buffer.toString('utf-8'), parse);
+      parser.parseString(buffer.toString('utf-8'), parse);
 
-			function parse(err, result) {
-				var data = converter(result),
-					path = getDestFilePath(dest, file);
-				grunt.log.oklns(file + ' -> ' + path);
-				writer(data, path);
-			}
-		}
+      function parse(err, result) {
+        var data = converter(result),
+          path = getDestFilePath(dest, file);
 
-		function converter(resx) {
-			var obj = {},
-				data = resx.root.data;
+        grunt.log.oklns(file + ' -> ' + path);
+        writer(data, path);
+      }
+    }
 
-			for (var i = 0; i < resx.root.data.length; i++) {
-				arrayToObject(
-					obj,
-					data[i].$.name.split(opts.delimiter),
-					data[i].value.toString());
-			}
-			return JSON.stringify(obj, null, 4);;
-		}
+    function converter(resx) {
+      var obj = {},
+        data = resx.root.data;
 
-		function arrayToObject(obj, keys, value) {
-			var last = arguments.length === 3 ? keys.pop() : false;
+      for (var i = 0; i < resx.root.data.length; i++) {
+        arrayToObject(
+          obj,
+          data[i].$.name.split(opts.delimiter),
+          data[i].value.toString());
+      }
+      return JSON.stringify(obj, null, 4);;
+    }
 
-			for (var i = 0; i < keys.length; i++) {
-				obj = obj[keys[i]] = obj[keys[i]] || {};
-			}
+    function arrayToObject(obj, keys, value) {
+      var last = arguments.length === 3 ? keys.pop() : false;
 
-			if (last) {
-				obj = obj[last] = value;
-			}
-			return obj;
-		}
+      for (var i = 0; i < keys.length; i++) {
+        obj = obj[keys[i]] = obj[keys[i]] || {};
+      }
 
-		function writer(contents, path) {
-			if (contents && path) {
-				grunt.file.write(path, contents);
-			} else {
-				grunt.log.errorlns('Couldn not write file: ' + path);
-			}
-		}
+      if (last) {
+        obj = obj[last] = value;
+      }
+      return obj;
+    }
 
-		function getDestFilePath(path, file) {
-			return dest + '/' +
-				getFilename(file)
-					.replace('.resx', '.' + (opts.extension || 'json'));
-		}
+    function writer(contents, path) {
+      if (contents && path) {
+        grunt.file.write(path, contents);
+      } else {
+        grunt.log.errorlns('Couldn not write file: ' + path);
+      }
+    }
 
-		function getFilename(path) {
-			return path.replace(/^.*[\\\/]/, '');
-		}
+    function getDestFilePath(path, file) {
+      return dest + '/' +
+        getFilename(file)
+          .replace('.resx', '.' + (opts.extension || 'json'));
+    }
 
-		function isResx(file) {
-			return /\.resx$/.test(file);
-		}
-	};
+    function getFilename(path) {
+      return path.replace(/^.*[\\\/]/, '');
+    }
+
+    function isResx(file) {
+      return /\.resx$/.test(file);
+    }
+  };
 };
